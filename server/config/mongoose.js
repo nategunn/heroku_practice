f1cf66049be3909465e053dbe21a7d4eb70054f3
Nate Gunn <nategunn2000@gmail.com>
2016-02-16 21:51:15 -0800

    adjusted mongoose.connect

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
 var uri
-mongoose.connect('mongodb://heroku_90bp8hpr:go18od19r4u9ejdea40sst7565@ds011218.mongolab.com:11218/heroku_90bp8hpr');
+mongoose.connect(' mongodb://<nategunn2000@gmail.com>:<L2u3c4y5B6>@ds011218.mongolab.com:11218/heroku_90bp8hpr');
 var models_path = __dirname + '/../models';
 fs.readdirSync(models_path).forEach(function(file) {
     if(file.indexOf('.js') > 0) {
